d3f184c9e514762c1848289df2905126b92f1dfe
Kevin Barabash <kevinb@khanacademy.org>
2017-06-05 23:07:31 -0400

    Fixes #6, add way to specify input/output paths to the command line tool

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const fs = require('fs');
+const {ArgumentParser} = require('argparse');
 
 const assembleProgram = require('./lib/assembler.js');
 const parse = require('./lib/parse.js');
 
-const code = fs.readFileSync('test.wast').toString();
+
+const parser = new ArgumentParser({
+  version: '0.0.1',
+  addHelp: true,
+  description: 'wasm-asm',
+});
+
+parser.addArgument('input');
+parser.addArgument(['-o', '--output']);
+
+const args = parser.parseArgs();
+
+const code = fs.readFileSync(args.input).toString();
 const program = parse(code);
 const output = assembleProgram(program);
 
 const buffer = new Uint8Array(output);
-fs.writeFileSync('build/manual.wasm', buffer, "binary");
+fs.writeFileSync(args.output, buffer, "binary");
