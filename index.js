4326d7e1c8258d38b35afaba7cb57db44dea4d50
Kevin Barabash <kevinb@khanacademy.org>
2017-06-07 00:37:11 -0400

    initial support for --verbose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {ArgumentParser} = require('argparse');
 
 const assembleProgram = require('./lib/assembler.js');
 const parse = require('./lib/parse.js');
-
+const print = require('./lib/print.js');
 
 const parser = new ArgumentParser({
   version: '0.0.1',
@@ -13,12 +13,17 @@ const parser = new ArgumentParser({
 
 parser.addArgument('input');
 parser.addArgument(['-o', '--output']);
+parser.addArgument(['--verbose'], {action: 'storeTrue', nargs: 0});
 
 const args = parser.parseArgs();
 
 const code = fs.readFileSync(args.input).toString();
 const program = parse(code);
 const output = assembleProgram(program);
+
+if (args.verbose) {
+    print(output);
+}
 
 const buffer = new Uint8Array(output);
 fs.writeFileSync(args.output, buffer, "binary");
